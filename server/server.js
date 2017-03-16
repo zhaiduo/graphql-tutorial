e9bbec68731587fe44f17490887cb0a4fc5c3c26
Jonas Helfer <jonas@helfer.email>
2017-03-15 17:41:58 -0700

    reorganize code

--- server/server.js
+++ server/server.js
@@ -1,24 +1,24 @@
+
 import express from 'express';
 import {
   graphqlExpress,
   graphiqlExpress,
 } from 'graphql-server-express';
-import {
-  makeExecutableSchema,
-  addMockFunctionsToSchema,
-} from 'graphql-tools';
 import bodyParser from 'body-parser';
 
-import { typeDefs } from './src/schema';
-import { resolvers } from './src/resolvers';
+import { schema } from './src/schema';
 
 const PORT = 4000;
-
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
 const server = express();
 
-server.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-server.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+server.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema
+}));
 
-server.listen(PORT, () => console.log(`GraphQL Server is now running on http://localhost:${PORT}`));
+server.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}));
+
+server.listen(PORT, () => 
+  console.log(`GraphQL Server is now running on http://localhost:${PORT}`)
+);
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,12 +1,21 @@
-// src/schema.js
-export const typeDefs = `
+import {
+  makeExecutableSchema,
+  addMockFunctionsToSchema,
+} from 'graphql-tools';
+
+import { resolvers } from './resolvers';
+
+const typeDefs = `
 type Channel {
-     id: ID!                # "!" denotes a required field
-          name: String
+  id: ID!                # "!" denotes a required field
+  name: String
 }
 # This type specifies the entry points into our API. In this case
 # there is only one - "channels" - which returns a list of channels.
 type Query {
-     channels: [Channel]    # "[]" means this is a list of channels
+  channels: [Channel]    # "[]" means this is a list of channels
 }
 `;
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+export { schema };
