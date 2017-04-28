4ebd46dca542e65dedca6a0c484d9b1f6c3444b1
Manur <gitHub@jungsfinest.de>
2017-04-28 22:45:29 +0200

    nix netowrk erros #8

--- server/server.js
+++ server/server.js
@@ -5,11 +5,14 @@ import {
   graphiqlExpress,
 } from 'graphql-server-express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import { schema } from './src/schema';
 
 const PORT = 4000;
 const server = express();
+
+server.use('*', cors({ origin: 'http://localhost:3000' }));
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({
   schema
