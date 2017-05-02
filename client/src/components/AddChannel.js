1f7c3f4354cf9a6efd242be181ba073316327ab5
Jonas Helfer <jonas@helfer.email>
2017-05-01 19:39:45 -0700

    Indicate optimistic state

--- client/src/components/AddChannel.js
+++ client/src/components/AddChannel.js
@@ -6,28 +6,25 @@ import { channelsListQuery } from './ChannelsListWithData';
 const AddChannel = ({ mutate }) => {
   const handleKeyUp = (evt) => {
     if (evt.keyCode === 13) {
-      evt.persist();
       mutate({ 
         variables: { name: evt.target.value },
         optimisticResponse: {
           addChannel: {
             name: evt.target.value,
-            id: Math.random(),
+            id: Math.round(Math.random() * -1000000),
             __typename: 'Channel',
           },
         },
-        update: (proxy, { data: { addChannel } }) => {
+        update: (store, { data: { addChannel } }) => {
             // Read the data from the cache for this query.
-            const data = proxy.readQuery({ query: channelsListQuery });
+            const data = store.readQuery({ query: channelsListQuery });
             // Add our channel from the mutation to the end.
             data.channels.push(addChannel);
             // Write the data back to the cache.
-            proxy.writeQuery({ query: channelsListQuery, data });
+            store.writeQuery({ query: channelsListQuery, data });
           },
-      })
-      .then( res => {
-        evt.target.value = '';  
       });
+      evt.target.value = '';
     }
   };
 
